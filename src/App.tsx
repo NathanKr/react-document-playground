e35bad4b1d4dcd676b3db15e9e1c06cd8f0a6ac9
Nathan Krasney <natankrasney@gmail.com>
2022-11-21 12:01:27 +0200

    use id

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
     console.log(document.getElementById("kkk")?.querySelectorAll("li").length);
   });
   const data =
-    "<ul id='kkk'><li>1111111111111</li><li>222222222222</li><li>33333333333</li><li>4444444444</li></ul>";
+    "<ul id='kkk'><li id='id1'>1111111111111</li><li id='id2'>222222222222</li><li id='id3'>33333333333</li><li id='id4'>4444444444</li></ul>";
 
   return (
     <div
       onClick={(e) => {
         console.log(
-          ((e.target as HTMLElement).nextElementSibling as HTMLElement)
-            .innerText
+          ((e.target as HTMLElement))
+            .id
         );
       }}
       dangerouslySetInnerHTML={{ __html: data }}
